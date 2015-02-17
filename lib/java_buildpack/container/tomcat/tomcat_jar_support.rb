5379d2768a21e33629c4bb135656f343d7619c96
alokhm <akshukla226@gmail.com>
2015-02-17 13:02:14 +0530

    Update tomcat_jar_support.rb

--- lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
+++ lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
@@ -50,8 +50,8 @@ module JavaBuildpack
        #"h3.zip"
 
 #pwd = Pathname.pwd + 'h3.zip'
-#pwd= tomcat_lib + 'h3.zip'
-pwd= tomcat_lib 
+pwd= tomcat_lib + 'h3.zip'
+#pwd= tomcat_lib 
 #zips = pwd.find_all {|p| p.fnmatch('h3.zip')}
 zips = pwd.find {|p| p.fnmatch('h3.zip')}
 jars = []
