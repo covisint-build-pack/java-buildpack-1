a17e8acf6845ef3c78d497d597a611cad8bd0dfb
alokhm <akshukla226@gmail.com>
2015-02-17 14:35:19 +0530

    Update tomcat_jar_support.rb

--- lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
+++ lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+require "pathname"
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/container'
 require 'java_buildpack/container/tomcat/tomcat_utils'
