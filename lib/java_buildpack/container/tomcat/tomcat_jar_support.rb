b1f1be61130128d25719ca1f8a9081e0669295b4
alokhm <akshukla226@gmail.com>
2015-02-17 15:55:59 +0530

    Update tomcat_jar_support.rb

--- lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
+++ lib/java_buildpack/container/tomcat/tomcat_jar_support.rb
@@ -28,7 +28,7 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-         download_jar_from_zip(jar_name, tomcat_lib)
+         download_jar(jar_name, tomcat_lib)
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
