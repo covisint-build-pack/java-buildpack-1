c9a6aa74acb3e7f4ff2688c9a1d51b7244d4eb5e
alokhm <akshukla226@gmail.com>
2015-02-18 15:22:38 +0530

    Update shared_lib_support.rb

--- lib/java_buildpack/container/tomcat/shared_lib_support.rb
+++ lib/java_buildpack/container/tomcat/shared_lib_support.rb
@@ -1,18 +1,6 @@
-# Encoding: utf-8
-# Cloud Foundry Java Buildpack
-# Copyright 2013 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Shared library support -we can upload any external project specific jars into tomcat sharedlib folder
+# Current s3 location: https://s3-us-west-2.amazonaws.com/covisint.com-shared-libs
+# This above s3 location we should upload one or more custom libs as a zip file
 
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/container'
@@ -23,7 +11,7 @@ module JavaBuildpack
   module Container
 
     # Encapsulates the detect, compile, and release functionality for Tomcat lifecycle support.
-    class TomcatJarSupport < JavaBuildpack::Component::VersionedDependencyComponent
+    class SharedLibSupport < JavaBuildpack::Component::VersionedDependencyComponent
       include JavaBuildpack::Container
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
@@ -42,10 +30,6 @@ module JavaBuildpack
         true
       end
 
-      private
-
-      
-
-end
+      end
 end
 end
