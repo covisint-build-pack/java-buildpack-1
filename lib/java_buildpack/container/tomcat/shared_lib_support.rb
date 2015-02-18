e45e3402a1673d5f343aa0c3fda9338cf5021f2a
alokhm <akshukla226@gmail.com>
2015-02-18 16:15:37 +0530

    Update shared_lib_support.rb

--- lib/java_buildpack/container/tomcat/shared_lib_support.rb
+++ lib/java_buildpack/container/tomcat/shared_lib_support.rb
@@ -1,6 +1,7 @@
 # Shared library support -we can upload any external project specific jars into tomcat sharedlib folder
 # Current s3 location: https://s3-us-west-2.amazonaws.com/covisint.com-shared-libs
 # This above s3 location we should upload one or more custom libs as a zip file
+# user should make entry of uploaded zip file into index.yml file
 
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/container'
