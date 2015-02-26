2dc31d0b394faa52790eb2dc29d68abd0b4d6157
alokhm <akshukla226@gmail.com>
2015-02-26 20:57:40 +0530

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -50,7 +50,7 @@ module JavaBuildpack
         [
           TomcatInstance.new(sub_configuration_context(context, 'tomcat')),
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
-          #YamlParser.new(context),
+          YamlParser.new(context),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
           TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support')),
           TomcatRedisStore.new(sub_configuration_context(context, 'redis_store')),
